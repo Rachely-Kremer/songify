0a3d3db90af649636600ef4b9acb76d306fcff24
Your Name <you@example.com>
2024-07-09 15:03:29 +0300

    add SignIn-SignUp component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,8 @@ import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MiniDrawer from './components/Navbar';
 import { Box } from '@mui/material';
+import SignIn from './components/SignIn/SignIn';
+import SignUp from './components/SignUp/SignUp';
 
 function App() {
   return (
@@ -9,7 +11,10 @@ function App() {
       <MiniDrawer />
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
         <Suspense fallback={<div>Loading...</div>}>
-          {/* <Chat /> */}
+          <Box sx={{ position: 'absolute', top: 16, right: 16, display: 'flex', gap: 2 }}>
+            <SignIn />
+            <SignUp />
+          </Box>
           <Routes>
             {/* <Route path="/" element={<HomeComp />} />
             <Route path="/search" element={<SearchComponent />} />
