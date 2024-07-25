4aaa4f124248669fa898b2702406414fe0e6c975
Your Name <you@example.com>
2024-07-25 20:57:24 +0300

    cleaning code 2

--- client/src/components/Song/SongComp.tsx
+++ client/src/components/Song/SongComp.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import AudioPlayer from 'react-h5-audio-player';
 import { useDispatch, useSelector } from 'react-redux';
 import 'react-h5-audio-player/lib/styles.css';
-import { addToPlaylist, createPlaylist, fetchPlaylistEntries } from '../../Redux/playlistSlice';
+import { fetchPlaylistEntries } from '../../Redux/playlistSlice';
 import { fetchSongs, updateView } from '../../Redux/songSlice';
 import { RootState, AppDispatch } from '../../Redux/store';
 import { Song } from '../../Types/song.type';
@@ -20,10 +20,6 @@ const SongComp: React.FC<SongCompProps> = ({ song }) => {
   const [localViews, setLocalViews] = useState<number>(song.views);
   const isFirstPlayRef = useRef<boolean>(true);
   const [isPlaying, setIsPlaying] = useState<boolean>(false);
-  const [currentSongIndex, setCurrentSongIndex] = useState(0);
-  const [selectedPlaylist, setSelectedPlaylist] = useState<string>('');
-  const [newPlaylistName, setNewPlaylistName] = useState<string>('');
-  const [isCreatingPlaylist, setIsCreatingPlaylist] = useState<boolean>(false);
 
   useEffect(() => {
     dispatch(fetchSongs());
@@ -34,25 +30,9 @@ const SongComp: React.FC<SongCompProps> = ({ song }) => {
     try {
       console.log(`Updating view for song ${songId} to ${views + 1}`);
       await dispatch(updateView({ id: songId, view: views + 1 })).unwrap();
-      setLocalViews(views + 1); 
+      setLocalViews(views + 1);
     } catch (error) {
       console.error('Error updating views:', error);
-    }
-  };
-
-  const handleAddToPlaylist = () => {
-    if (selectedPlaylist) {
-      dispatch(addToPlaylist({ songId: song._id, playlistId: selectedPlaylist }));
-    } else {
-      setIsCreatingPlaylist(true);
-    }
-  };
-
-  const handleCreatePlaylist = () => {
-    if (newPlaylistName) {
-      dispatch(createPlaylist(newPlaylistName));
-      setNewPlaylistName('');
-      setIsCreatingPlaylist(false);
     }
   };
 
@@ -73,36 +53,15 @@ const SongComp: React.FC<SongCompProps> = ({ song }) => {
       {songStatus === 'loading' ? (
         <p>Loading songs...</p>
       ) : songStatus === 'succeeded' ? (
-        <>
-          <AudioPlayer
-            src={song.songUrl}
-            onPlay={handlePlayPause}
-            onPause={handlePlayPause}
-            showSkipControls={true}
-            showJumpControls={false}
-            autoPlayAfterSrcChange={true}
-            autoPlay
-          />
-          {isCreatingPlaylist && (
-            <div>
-              <input
-                type="text"
-                value={newPlaylistName}
-                onChange={(e) => setNewPlaylistName(e.target.value)}
-                placeholder="Enter new playlist name"
-              />
-              <button onClick={handleCreatePlaylist}>Create Playlist</button>
-            </div>
-          )}
-          {playlistEntries.length > 0 && (
-            <select onChange={(e) => setSelectedPlaylist(e.target.value)} value={selectedPlaylist}>
-              <option value="">Select Playlist</option>
-              {playlistEntries.map((playlist) => (
-                <option key={playlist._id} value={playlist._id}>{playlist.name}</option>
-              ))}
-            </select>
-          )}
-        </>
+        <AudioPlayer
+          src={song.songUrl}
+          onPlay={handlePlayPause}
+          onPause={handlePlayPause}
+          showSkipControls={true}
+          showJumpControls={false}
+          autoPlayAfterSrcChange={true}
+          autoPlay
+        />
       ) : (
         <p>Failed to load songs.</p>
       )}
